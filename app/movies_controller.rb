a3a4cd8c125eb8d6fcec5d9ea6ee0f40972fb4c0
Joseph Parker <jparker010190@gmail.com>
2018-05-05 22:27:56 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(args = nil)
   Movie.create do |m|
-    m.title  = args[title:]
-    m.release_date = args[release_date:]
+    m.title  = args[title]
+    m.release_date = args[release_date]
   end
 end
 
