0c4a1f6b2a8b3befb7bd98d3e3d60fb253f43769
Joseph Parker <jparker010190@gmail.com>
2018-05-05 22:23:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = nil)
+def can_be_created_in_a_block(args = title:,release_date:)
   Movie.create do |m|
     m
   end
